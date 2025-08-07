d92002c687767d188e69e116718139f002234bdd
eki-git <eramcilovic@gmail.com>
2025-08-07 17:05:47 +0200

    added logo header footer

--- src/js/Pages/Home.js
+++ src/js/Pages/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
+import Header from "../Components/Header/Header";
+import Footer from "../Components/Footer/Footer";
 
 const Home = () => {
   return (
       <>
+          <Header type="header" />
         <p>HomePage</p>
+          <Footer type="footer" />
       </>
   )
 }
