fcd1893d28b8ed2d921a961d4ac1d0892ceebf1e
eki-git <eramcilovic@gmail.com>
2025-08-11 18:53:00 +0200

    added banner

--- src/js/Components/App.js
+++ src/js/Components/App.js
@@ -1,27 +1,32 @@
 import React from "react";
-// import Home from "../Pages/Home";
-import Logo from "./Logo/Logo";
-import Menu from "./Menu/Menu";
-import Topline from "./Topline/Topline";
+ import Home from "../Pages/Home";
+ import Logo from "./Header/Logo/Logo";
+ import Menu from "./Header/Menu/Menu";
+ import Topline from "./Header/Topline/Topline";
+ import Footer from "./Footer/Footer";
+ import Banner from "./Banner/Banner";
 
 const App = () => {
     return (
         <>
+             <Topline/>
+             <div className="header">
+                 <Logo/>
+                 <Menu type="header__nav"
+                       items={
+                           [
+                               { title: "Domov", url: "#" },
+                               { title: "Zakaj mi", url: "#about" },
+                               { title: "Osebni prevoz", url: "#stack" },
+                               { title: "Razišči", url: "#projects" },
+                               { title: "Kontakt", url: "#contact" },
+                           ]
+                       }/>
+             </div>
             {/*<Home/>*/}
-            <Topline/>
-            <div className="header">
-                <Logo/>
-                <Menu type="header__nav"
-                      items={
-                          [
-                              { title: "Domov", url: "#" },
-                              { title: "Zakaj mi", url: "#about" },
-                              { title: "Osebni prevoz", url: "#stack" },
-                              { title: "Razišči", url: "#projects" },
-                              { title: "Kontakt", url: "#contact" },
-                          ]
-                      }/>
-            </div>
+            <Banner />
+
+            <Footer />
         </>
     )
 }
---
+++ src/js/Components/Banner/Banner.js
@@ -0,0 +1,28 @@
+import React from "react";
+import "./_banner.scss";
+import main from "../../../assets/mainpic.png";
+
+const Banner = () => {
+    return (
+        <section className="banner"
+        style={{ backgroundImage: `url(${main})` }}>
+            <div className="banner__overlay">
+                <h1>UGOTOVITE, ZAKAJ SMO #1 ZANESLJIV SELITVENI SERVIS<br/>
+                    Smo selitveno podjetje z izkušenimi strokovnjaki,<br/>
+                    ki vsako selitev opravijo skrbno, varno in učinkovito.
+                    <span className="gradient">Naš cilj</span><br/>
+                    je, da je vaša selitev brez stresa – z ljubeznijo,
+                    pozornostjo do podrobnosti in popolno predanostjo vašim potrebam.
+                    Od prvega koraka do zadnjega kosa pohištva poskrbimo, da vse poteka gladko in brez skrbi.
+                </h1>
+                <button className="banner__btn">
+                    PRIDOBITE BREZPLAČNO PONUDBO
+                </button>
+            </div>
+            {/*<div className="banner__image">*/}
+            {/*     <img src={main} alt="Main"/>*/}
+            {/*</div>*/}
+        </section>
+    )
+}
+export default Banner;
