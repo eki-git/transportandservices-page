96300aa748e8bbd7aed21b255f5baf8ded369a60
eki-git <eramcilovic@gmail.com>
2025-08-27 02:14:32 +0200

    added Container js adn App js update with scss

--- src/js/Components/App.js
+++ src/js/Components/App.js
@@ -1,37 +1,25 @@
 import React from "react";
-import Topline from "./Header/Topline/Topline";
-import Logo from "./Header/Logo/Logo";
-import Menu from "./Header/Menu/Menu";
+import Container from "./Container/Container";
+import Header from "./Header/Header";
 import Banner from "./Banner/Banner";
 import Services from "./Services/Services";
 import FormSection from "./FormSection/FormSection";
 import References from "./References/References";
 import { reference } from "./References/reference";
 import Footer from "./Footer/Footer";
+import "../../scss/layout/_main.scss";
+
 
 const App = () => {
     return (
-        <>
-             <Topline/>
-                 <header className="header">
-                     <Logo/>
-                     <Menu type="header__nav"
-                           items={
-                               [
-                                   { title: "Domov", url: "#" },
-                                   { title: "Zakaj mi", url: "#about" },
-                                   { title: "Osebni prevoz", url: "#stack" },
-                                   { title: "Razišči", url: "#projects" },
-                                   { title: "Kontakt", url: "#contact" },
-                               ]
-                           }/>
-                 </header>
-            <Banner />
-            <Services />
-            <FormSection />
-            <References reference={reference} />
-            <Footer />
-        </>
+        <Container>
+           <Header type="header" />
+           <Banner />
+           <Services />
+           <FormSection />
+           <References reference={reference} />
+           <Footer />
+        </Container>
     );
 };
 
